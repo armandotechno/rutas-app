5114b89ccf11bc19d054f052da6d6321d6c49e7d
Armando Capriles <armandotechno12@gmail.com>
2023-03-30 14:31:33 -0400

    Agregando Iconos y el navegation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native"
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const App = () => {
     return (
       <View>
         <Text>Hola Mundo</Text>
+        <Icon name="star-outline" size={ 24 } color="blue" />
       </View>
     )
 }
