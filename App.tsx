2275fb17aa60a9dcb99a99bd31484e1e92bc0c68
Armando Capriles <armandotechno12@gmail.com>
2023-03-31 23:39:28 -0400

    Permission Provider - ContextAPI

--- App.tsx
+++ App.tsx
@@ -2,11 +2,24 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { Navigator } from './src/navigator/Navigator';
+import { PermissionsProvider } from './src/context/PermissionsContext';
+
+const AppState = ({ children }: { children: JSX.Element | JSX.Element[] })=> {
+
+  return (
+    <PermissionsProvider>
+      { children }
+    </PermissionsProvider>
+  )
+
+}
 
 const App = () => {
     return (
       <NavigationContainer>
-        <Navigator />
+        <AppState>
+          <Navigator />
+        </AppState>
       </NavigationContainer>
     )
 }
--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { PermissionStatus } from "react-native-permissions";
 
 
@@ -10,13 +10,31 @@ export const permissionInitState: PermissionState = {
     locationStatus: 'unavailable',
 }
 
-export const PermissionsContext = createContext({});
+type PermissionsContextProps = {
+    permissions: PermissionState;
+    askLocationPermission: () => void;
+    checkLocationPermission: () => void;
+}
+
+export const PermissionsContext = createContext({} as PermissionsContextProps);
 
 export const PermissionsProvider = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+
+    const askLocationPermission = () => {
+
+    }
+    const checkLocationPermission = () => {
+
+    }
     
+    const [ permissions, setPermissions ] = useState( permissionInitState )
 
     return (
-        <PermissionsContext.Provider value={{}}>
+        <PermissionsContext.Provider value={{
+            permissions,
+            askLocationPermission,
+            checkLocationPermission
+        }}>
             { children }
         </PermissionsContext.Provider>
     )
