1ddd3fa2d18bada18ab5787217119db7282e8d9a
Armando Capriles <armandotechno12@gmail.com>
2023-04-02 13:17:39 -0400

    Detectar qué pantalla mostrar al inicio de forma automática

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 
 export const LoadingScreen = () => {
     return (
-      <View>
-        <Text>Loading Screen</Text>
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <ActivityIndicator 
+          size={ 30 }
+          color="Black"
+        />
       </View>
     )
 }
